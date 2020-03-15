81fdac97174a004feeebefc39221b2419c55aa2a
jodyphelan <jody.phelan@lshtm.ac.uk>
2020-03-15 13:11:32 +0000

    set version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='covid-profiler',
-    version='0.1.6',
+    version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
