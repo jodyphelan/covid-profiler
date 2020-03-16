0abd2f16e460d49bffb23bf7cd5e290ea95f1ade
jodyphelan <jody.phelan@lshtm.ac.uk>
2020-03-16 22:14:47 +0000

    add annotation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='covid-profiler',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
