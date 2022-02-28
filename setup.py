ec44d3ab70855a7956130119957a9edda52c02dc
jodyphelan <jody.phelan@lshtm.ac.uk>
2022-02-28 21:59:19 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 import os
 setup(
     name='covid-profiler',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
